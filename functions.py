08ea9b2309aec64dd0cbe6951382bf530dcd393c
brian506 <brianmeng6@gmail.com>
2025-04-11 20:35:10 -0400

    changes

--- functions.py
+++ functions.py
@@ -1,56 +1,87 @@
+"""
+This file contains implementation of the factorial, fibonacci, and greatest 
+common divisor (GCD) functions using both iterative and recursive approaches. 
+"""
 
-class functions:
-    def factorial_iterative(n):
-        result = 1
-        if n == 0:
-            return result
-        else:
-            for i in range(1, n + 1):
-                result *= i
-            return result
+def factorial_iterative(n):
+    """
+    Function that returns the factorial of a number n using an iterative 
+    approach. It iterates from 1 to n, multiplying the result (initially 1) by
+    each number in the range. If n is 0, it returns 0. Otherwise, it returns 
+    the factorial of n.
+    """
+    result = 1
+    if n == 0:
+        return 0
+    else:
+        for i in range(1, n + 1):
+            result *= i
+        return result
+    
+def factorial_recursive(n):
+    """
+    A function that returns the factorial of a number n using a recursive 
+    approach. It calls itself with n-1 until n is 0, multiplying the current 
+    value n with the value of the factorial of n-1. Once it reaches n is 0,
+    it returns 1. 
+    """
+    if n == 0:
+        return 1
+    else: 
+        return n * factorial_recursive(n-1)
+    
+# fibonacci functions
+def fibonacci_iterative(n):
+    """
+    A function that returns the nth number in the fibonacci sequence using an 
+    iterative approach. It creates two variables a and b, intialized to 0 and 1.
+    It then iterates from 2 to n, updating the value of a to b and b to a+b. 
+    Once it reaches n, it returns b. If n <= 0, it returns an error message. 
+    If n is 1, 0 is returned, and if n is 2, 1 is returned.
+    """
+    if n < 0:
+        return 'Must be not be negative!'
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else: 
+        a, b = 0, 1
+        for i in range(2, n):
+            a, b = b, a + b
+        return b
 
-    def factorial_recursive(n):
-        if n == 0:
-            return 1
-        else: 
-            return n * functions.factorial(n-1)
-        
-    def fibonacci_iterative(n):
-        if n <= 0:
-            return 'Must be greater than 0!'
-        elif n == 1:
-            return 0
-        elif n == 2:
-            return 1
-        else: 
-            a, b = 0, 1
-            for i in range(2, n):
-                a, b = b, a + b
-            return b
+def fibonacci_recursive(n): # Function that returns nth number in the fibonacci sequence
+    """
+    A function that returns the nth number in the fibonacci sequence using a 
+    recursive approach. It calls itself using n-1 and n-2, adding the two 
+    results together until the function reaches a base case of n <= 1. In this
+    case, it returns n, which is either 0 or 1. The function throws an error 
+    message when n < 0.
+    """
+    if n < 0:
+        return 'Must be not be negative!'
+    elif n <= 1:
+        return n
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+    
+# gcd functions
+def gcd_iterative(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else: 
+        while b != 0:
+            a, b = b, a % b
+        return a
+    
+def gcd_recursive(a, b): # Function that returns the greatest common divisor of two numbers
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return gcd_recursive(b, a % b)
 
-    def fibonacci_recursive(n): # Function that returns nth number in the fibonacci sequence
-        if n <= 1:
-            return n
-        else:
-            return functions.fibonacci(n-1) + functions.fibonacci(n-2)
-        
-    def gcd_iterative(a, b):
-        if a == 0:
-            return b
-        elif b == 0:
-            return a
-        else: 
-            while b != 0:
-                a, b = b, a % b
-            return a
-        
-    print(gcd_iterative(15, 10))
-        
-    def gcd_recursive(a, b): # Function that returns the greatest common divisor of two numbers
-        if a == 0:
-            return b
-        elif b == 0:
-            return a
-        else:
-            return functions.gcd(b, a % b)
-        
