699bf15adc59e27206797870ed05386be0252340
brian506 <brianmeng6@gmail.com>
2025-04-14 23:03:45 -0400

    changes

--- functions.py
+++ functions.py
@@ -41,10 +41,8 @@ def fibonacci_iterative(n):
     """
     if n < 0:
         return 'Must be not be negative!'
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return 1
+    elif n == 0 or n == 1:
+        return n
     else: 
         a, b = 0, 1
         for i in range(2, n):
